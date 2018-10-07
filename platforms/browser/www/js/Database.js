c3820055362e1cc95a66759d10dc58f51a0794bc
Patrick Farrell <pfarrell415@gmail.com>
2018-10-06 23:45:44 -0700

    functioning search on browser and android

--- platforms/browser/www/js/Database.js
+++ platforms/browser/www/js/Database.js
@@ -1,3 +1,4 @@
+var minResults = 5;
 
 function Database()
 {
@@ -6,7 +7,7 @@ function Database()
       async: false
   });
 
-  $.getJSON("www/index_files/plants.json", function(data){
+  $.getJSON("index_files/plants.json", function(data){
     temp = data;
   });
 
@@ -14,8 +15,39 @@ function Database()
       async: true
   });
 
-  var temp;
   this.elements = temp;
+}
+
+Database.prototype.search = function search(attributesAsString)
+{
+  var targetAttributes = attributesAsString.split(",")
+  var givenAttributes = [];
+  var results = [];
+
+  for(i = 0; i < this.elements.length; i++)
+  {
+      var score = 0;
+      givenAttributes = this.elements[i].slice(2);
+      for(n = 0; n < targetAttributes.length; n++)
+        if(givenAttributes.includes(targetAttributes[n]))
+          score++;
+
+      if(results.length < minResults)
+        results[results.length] = [score, i];
+      else
+      {
+        results.sort()
+        if(results[0][0] < score)
+          results[0] = [score, i];
+      }
+  }
+
+  temp = results.slice(0);
+  console.log(temp);
+
+  for(i = 0; i < results.length; i++)
+    results[i] = [results[i][0], this.elements[results[i][1]][0]]
+  return results;
 }
 
 Database.prototype.get = function get(name)
